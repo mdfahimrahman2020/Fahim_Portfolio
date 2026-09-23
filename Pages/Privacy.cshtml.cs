87290d8da868a736999539a0487e252ba70ba246
Md.Fahim Rahman <114246825+mdfahimrahman2020@users.noreply.github.com>
2026-09-23 15:34:50 +0600

    Add project files.

---
+++ Pages/Privacy.cshtml.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Fahim_Portfolio.Pages
+{
+    public class PrivacyModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+
+}
